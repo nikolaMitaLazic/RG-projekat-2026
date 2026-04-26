fd81b02ec1ec7549c7638f562de7b161e856e220
Nikola Lazić <mitalazic@gmail.com>
2026-04-26 16:59:33 +0200

    feat: create and register empty main controller

---
+++ app/include/MainController.hpp
@@ -0,0 +1,15 @@
+//
+// Created by root on 4/26/26.
+//
+
+#ifndef MATF_RG_PROJECT_MAINCONTROLLER_HPP
+#define MATF_RG_PROJECT_MAINCONTROLLER_HPP
+#include <engine/core/Controller.hpp>
+
+namespace app {
+    class MainController : public engine::core::Controller {
+        void initialize() override;
+    };
+}
+
+#endif //MATF_RG_PROJECT_MAINCONTROLLER_HPP
---
+++ app/src/MainController.cpp
@@ -0,0 +1,8 @@
+#include <MainController.hpp>
+#include <spdlog/spdlog.h>
+
+namespace app {
+    void MainController::initialize() {
+        spdlog::info("MainController: Successful initialization!");
+    }
+}
