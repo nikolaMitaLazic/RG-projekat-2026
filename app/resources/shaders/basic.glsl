fcf5c64d244a8e06e77a8003b9e027dd30989fc5
Nikola Lazić <mitalazic@gmail.com>
2026-08-08 15:12:31 +0200

    feat: add moonlight (directional light)

--- app/resources/shaders/basic.glsl
+++ app/resources/shaders/basic.glsl
@@ -16,7 +16,7 @@ uniform mat4 projection;
 void main()
 {
     FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = aNormal;
+    Normal = mat3(transpose(inverse(model))) * aNormal;
     TexCoords = aTexCoords;
     gl_Position = projection * view * vec4(FragPos, 1.0);
 }
@@ -27,9 +27,34 @@ void main()
 out vec4 FragColor;
 
 in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
+
+struct DirectionalLight {
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
 
 uniform sampler2D texture_diffuse1;
+uniform DirectionalLight directionalLight;
+uniform vec3 viewPosition;
 
 void main() {
-    FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb, 1.0);
+    vec3 color = texture(texture_diffuse1, TexCoords).rgb;
+    vec3 normal = normalize(Normal);
+    vec3 lightDirection = normalize(-directionalLight.direction);
+
+    vec3 ambient = directionalLight.ambient * color;
+
+    float diffuseStrength = max(dot(normal, lightDirection), 0.0);
+    vec3 diffuse = directionalLight.diffuse * diffuseStrength * color;
+
+    vec3 viewDirection = normalize(viewPosition - FragPos);
+    vec3 reflectedDirection = reflect(-lightDirection, normal);
+    float specularStrength = pow(max(dot(viewDirection, reflectedDirection), 0.0), 32.0);
+    vec3 specular = directionalLight.specular * specularStrength;
+
+    FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -136,6 +136,16 @@ namespace app {
     }
 
     void MainController::draw() {
+        auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        auto shader = resources->shader("basic");
+        shader->use();
+        shader->set_vec3("directionalLight.direction", glm::vec3(-0.45f, -1.0f, 0.7f));
+        shader->set_vec3("directionalLight.ambient", glm::vec3(0.18f, 0.20f, 0.28f));
+        shader->set_vec3("directionalLight.diffuse", glm::vec3(0.45f, 0.50f, 0.65f));
+        shader->set_vec3("directionalLight.specular", glm::vec3(0.30f, 0.35f, 0.45f));
+        shader->set_vec3("viewPosition", graphics->camera()->Position);
+
         draw_hen();
         draw_tree();
         draw_road();
